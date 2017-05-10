31e151bcaa19670fb0858f3635eaf31373ae352e
Lane Faison <lane.faison@icloud.com>
2017-05-10 16:51:43 -0600

    Exercise complete

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet var viewScreen: UIView!
+    @IBOutlet weak var pillImage: UIImageView!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryTF: UITextField!
+    @IBOutlet weak var zipLabel: UILabel!
+    @IBOutlet weak var zipTF: UITextField!
+    
+    let states = ["Alaska","California","Colorado","New Jersey","New York","Texas"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        countryLabel.isHidden = true
+        countryTF.isHidden = true
+        zipLabel.isHidden = true
+        zipTF.isHidden = true
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 //refers to the number of columns we want
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal) // Changes label title to the picked state
+        statePicker.isHidden = true
+        countryLabel.isHidden = false
+        countryTF.isHidden = false
+        zipLabel.isHidden = false
+        zipTF.isHidden = false
+    }
+    
+    @IBAction func buyTapped(_ sender: Any) {
+        viewScreen.isHidden = true
+    }
+    
 }
 
